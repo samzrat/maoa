4ca9f068074984911148337c25c1277220d612f4
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2013-02-02 23:04:46 -0800

    corrected maven build

--- src/net/vikasta/maoa/App.java
+++ src/net/vikasta/maoa/App.java
@@ -1,9 +1,6 @@
 package net.vikasta.maoa;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
---
+++ src/net/vikasta/maoa/algorithm/PositionEvent.java
@@ -0,0 +1,28 @@
+package net.vikasta.maoa.algorithm;
+
+
+public class PositionEvent 
+{
+	final String sid; //sensor id
+	final long x, y, z; // position in mm
+	final long speed;  //micrometer/sec
+	final long vx, vy, vz; //direction vector with size of 10,000
+	
+
+	
+	public PositionEvent(String sid, long x, long y, long z, long speed, long vx, long vy, long vz)
+	{
+		this.sid = sid;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.speed = speed;
+		this.vx = vx;
+		this.vy = vy;
+		this.vz = vz;
+	}
+/*	public double speedX()
+	{
+		return speed*vx*Math.Math.
+	}*/
+}
