5c687013c375c63e40b9f3aa69d55138e9b88160
Sameer <sameersamarthp@gmail.com>
2013-02-05 22:29:09 -0600

    added storm examples

--- src/main/java/playground/storm/TopologyMain.java
+++ src/main/java/playground/storm/TopologyMain.java
@@ -1,8 +1,34 @@
 package main.java.playground.storm;
 
+import main.java.playground.storm.spouts.WordReader;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import main.java.playground.storm.bolts.WordCounter;
+import main.java.playground.storm.bolts.WordNormalizer;
+
 
 public class TopologyMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
          
+        //Topology definition
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("word-reader",new WordReader(),5);
+		builder.setBolt("word-normalizer", new WordNormalizer(),5)
+			.shuffleGrouping("word-reader");
+		builder.setBolt("word-counter", new WordCounter(),1)
+			.fieldsGrouping("word-normalizer", new Fields("word"));
+		
+        //Configuration
+		Config conf = new Config();
+		conf.put("wordsFile", args[0]);
+		conf.setDebug(false);
+        //Topology run
+		conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("Getting-Started-Toplogie", conf, builder.createTopology());
+		Thread.sleep(1000);
+		cluster.shutdown();
 	}
 }
