62e7d38710a28233ba9af299d3a67afa17fefd05
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2014-02-10 01:51:06 -0800

    basic house pubsub

--- debs2014/src/main/scala/debs2014/HouseActor.scala
+++ debs2014/src/main/scala/debs2014/HouseActor.scala
@@ -7,26 +7,48 @@ import akka.actor.Actor
 import akka.cluster.ClusterEvent.MemberEvent
 import akka.cluster.ClusterEvent.UnreachableMember
 import akka.actor.Props
+import akka.contrib.pattern._
 
 object HouseActor {
-  def props(plugIds: Set[String]): Props =
-    Props(classOf[HouseActor], plugIds)
+  def props(plugIds: Set[String]): Props = Props(classOf[HouseActor], plugIds)
+  
+  case class PublishedMsg(content: String)
 }
 
 class HouseActor(plugIds: Set[String]) extends Actor with ActorLogging {
 
   plugIds.foreach(plugId => context.actorOf(Props(classOf[PlugActor], plugId), name = plugId))
   
+  import DistributedPubSubMediator.{ Subscribe, SubscribeAck, Publish }
+  val mediator = DistributedPubSubExtension(context.system).mediator
+  // subscribe to the topic named "content"
+  mediator ! Subscribe("self.path.toStringWithoutAddress", self)
+  log.info("(self.path.toStringWithoutAddress = " + self.path.toStringWithoutAddress)
+  mediator ! Publish("self.path.toStringWithoutAddress", "howdy")
   
-
+  def ready: Actor.Receive = {
+    case s: String ⇒
+      log.info("Got {}", s)
+  }
+  
   def receive = {
-    case MemberUp(member) =>
-      log.info("Member is Up: {}", member.address)
-    case UnreachableMember(member) =>
-      log.info("Member detected as unreachable: {}", member)
-    case MemberRemoved(member, previousStatus) =>
-      log.info("Member is Removed: {} after {}",
-        member.address, previousStatus)
-    case _: MemberEvent => // ignore
+    case SubscribeAck(Subscribe("self.path.toStringWithoutAddress", self)) ⇒ {
+      log.info("SubscribeAck received")
+      //context become ready
+      for (i <- 1 to 5) 
+      {
+        log.info("Publishing msg {}", i)
+        //mediator ! Publish("self.path.toStringWithoutAddress", HouseActor.PublishedMsg("howdy"))
+        mediator ! Publish("self.path.toStringWithoutAddress", "howdy")
+      }  
+      Thread.sleep(10000)
+    }  
+    case in: String =>
+      log.info("Pubished message received {} sender is {}", in, sender.path)
+      //mediator ! Publish("self.path.toStringWithoutAddress", "howdy Ranganath")
+    case HouseActor.PublishedMsg(content) =>
+      log.info("Pubished message received {} sender is {}", content, sender.path)
+
+    case _ => throw new Exception
   }
 }
